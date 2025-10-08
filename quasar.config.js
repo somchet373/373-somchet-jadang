94c3d760ace23701a33971818816e6a9750da559
somchet373 <somchet2005@gmail.com>
2025-10-08 11:12:33 +0700

    http true

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#devserver
     devServer: {
-      // https: true,
+      https: true,
       open: true // opens browser window automatically
     },
 
