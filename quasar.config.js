62f5c548cd9be0c9e2d8a56506558574de99e952
somchet373 <somchet2005@gmail.com>
2025-10-08 10:00:46 +0700

    fix html

--- quasar.config.js
+++ quasar.config.js
@@ -44,6 +44,11 @@ export default defineConfig((/* ctx */) => {
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
       publicPath: '/373-somchet-jadang/',
+
+      htmlVariables: {
+        productName: '373 Somchet Jadang',
+        productDescription: 'Quasar test project deployed to GitHub Pages'
+      }
       // analyze: true,
       // env: {},
       // rawDefine: {}
